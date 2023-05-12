589b69f8640b66f25288b7b881e056ec1d8827d1
Raymond-techy <adunoloyerahmantaiwo200208@gmail.com>
2023-05-12 09:26:28 -0700

    overflow solved

--- src/Pages/Cleaning.js
+++ src/Pages/Cleaning.js
@@ -11,91 +11,95 @@ const texts = [
 
 const Cleaning = () => {
   return (
-    <div>
-      <div className="mt-24">
-        <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
-          <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
-            <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
-              <span className="mb-6 sm:mx-auto">
-                <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
-                  <svg
-                    className="w-10 h-10 text-deep-orange-accent-400"
-                    stroke="currentColor"
-                    viewBox="0 0 52 52">
-                    <polygon
-                      strokeWidth="3"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      fill="none"
-                      points="29 13 14 29 25 29 23 39 38 23 27 23"
-                    />
-                  </svg>
-                </div>
-              </span>
-              <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
-                <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-                  <span className="relative inline-block">
-                    <svg
-                      viewBox="0 0 52 24"
-                      fill="currentColor"
-                      className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
-                      <defs>
-                        <pattern
-                          id="e77df901-b9d7-4b9b-822e-16b2d410795b"
-                          x="0"
-                          y="0"
-                          width=".135"
-                          height=".30">
-                          <circle cx="1" cy="1" r=".7" />
-                        </pattern>
-                      </defs>
-                      <rect
-                        fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
-                        width="52"
-                        height="24"
-                      />
-                    </svg>
-                    <span className="relative">Store </span>
+    <div className="overflow-hidden">
+      <div className="overflow-x-hidden">
+        <div>
+          <div className="mt-24">
+            <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
+              <div className="max-w-xl sm:mx-auto lg:max-w-2xl">
+                <div className="flex flex-col mb-16 sm:text-center sm:mb-0">
+                  <span className="mb-6 sm:mx-auto">
+                    <div className="flex items-center justify-center w-12 h-12 rounded-full bg-indigo-50">
+                      <svg
+                        className="w-10 h-10 text-deep-orange-accent-400"
+                        stroke="currentColor"
+                        viewBox="0 0 52 52">
+                        <polygon
+                          strokeWidth="3"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          fill="none"
+                          points="29 13 14 29 25 29 23 39 38 23 27 23"
+                        />
+                      </svg>
+                    </div>
                   </span>
-                  For a quick Cleaning
-                </h2>
-                <p className="text-base text-gray-700 md:text-lg">
-                  We offer the best cleaning services in a secured environment
-                  with better hygiene using the appropriate working instrument
-                  at an affordable price
-                </p>
+                  <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
+                    <h2 className="max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
+                      <span className="relative inline-block">
+                        <svg
+                          viewBox="0 0 52 24"
+                          fill="currentColor"
+                          className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block">
+                          <defs>
+                            <pattern
+                              id="e77df901-b9d7-4b9b-822e-16b2d410795b"
+                              x="0"
+                              y="0"
+                              width=".135"
+                              height=".30">
+                              <circle cx="1" cy="1" r=".7" />
+                            </pattern>
+                          </defs>
+                          <rect
+                            fill="url(#e77df901-b9d7-4b9b-822e-16b2d410795b)"
+                            width="52"
+                            height="24"
+                          />
+                        </svg>
+                        <span className="relative">Store </span>
+                      </span>
+                      For a quick Cleaning
+                    </h2>
+                    <p className="text-base text-gray-700 md:text-lg">
+                      We offer the best cleaning services in a secured
+                      environment with better hygiene using the appropriate
+                      working instrument at an affordable price
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
+
+          <Content2
+            target="/services/dryCleaning"
+            title="House Cleaning"
+            text={texts[1]}
+            image={
+              "https://images.unsplash.com/photo-1527694224012-be005121c774?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhvdXNlJTIwY2xlYW5pbmd8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            }
+          />
+          <Content
+            target="/services/dryCleaning"
+            title="Office Cleaning"
+            text={texts[0]}
+            image={
+              "https://images.unsplash.com/photo-1579487785973-74d2ca7abdd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2xlYW5pbmclMjBvZmZpY2V8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
+            }
+          />
+
+          <Content2
+            target="/services/dryCleaning"
+            title="Clothes Dry Cleaning"
+            text={texts[2]}
+            image={
+              "https://images.unsplash.com/photo-1489274495757-95c7c837b101?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNsZWFuaW5nfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+            }
+          />
+          <Footer />
         </div>
       </div>
-
-      <Content2
-        target="/services/dryCleaning"
-        title="House Cleaning"
-        text={texts[1]}
-        image={
-          "https://images.unsplash.com/photo-1527694224012-be005121c774?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGhvdXNlJTIwY2xlYW5pbmd8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <Content
-        target="/services/dryCleaning"
-        title="Office Cleaning"
-        text={texts[0]}
-        image={
-          "https://images.unsplash.com/photo-1579487785973-74d2ca7abdd5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2xlYW5pbmclMjBvZmZpY2V8ZW58MHwxfDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-
-      <Content2
-        target="/services/dryCleaning"
-        title="Clothes Dry Cleaning"
-        text={texts[2]}
-        image={
-          "https://images.unsplash.com/photo-1489274495757-95c7c837b101?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fGNsZWFuaW5nfGVufDB8MXwwfHw%3D&auto=format&fit=crop&w=500&q=60"
-        }
-      />
-      <Footer />
     </div>
   );
 };
